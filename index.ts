f6eb91438ba91a600e571f963846033ba5b8e777
Gassan <g_fox_fts@yahoo.com>
2020-10-27 20:34:53 +0100

    BehaviorSubject

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import './style.css';
 console.clear();
 // begin lesson code
 
-import { Subject, interval } from 'rxjs';
+import { BehaviorSubject, interval } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 const observer = {
@@ -11,12 +11,39 @@ const observer = {
   complete: () => console.log('complete')
 };
 
-const subject = new Subject();
-const interval$ = interval(2000).pipe(
-  tap(i => console.log('new interval', i))
-);
+/*
+ * BehaviorSubject's accept an argument, the initial seed value.
+ * This value will be emitted to subscribers until .next is called
+ * again. New subscribers to BehaviorSubject's always receieve the
+ * last emitted value on subscription.
+ */
+const subject = new BehaviorSubject('Hello');
 
-interval$.subscribe(subject);
-
+/*
+ * Subscribers to the BehaviorSubject will receieve the seed value,
+ * or last emitted value. In this case no other value has been
+ * emitted so the subscriber will initially receive 'Hello'
+ */
 const subscription = subject.subscribe(observer);
+
+/*
+ * Emit 'World' to all subscribers, just the observer above
+ * right now.
+ */
+subject.next('World');
+
+/*
+ * Contrary to the normal Subject, BehaviorSubject will deliver the last
+ * emitted value to late subscribers. In this case our subscriber
+ * will receive 'World' immediately.
+ */
 const secondSubscription = subject.subscribe(observer);
+
+subject.next('Goodbye!');
+
+/*
+ * You can also access the current value of the BehaviorSubject
+ * synchronously by calling getValue(), although this is
+ * generally not advised.
+ */
+console.log('getValue()', subject.getValue());
