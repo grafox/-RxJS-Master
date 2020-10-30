9712f60a27647bd4c5db224195d7db6bf50774d3
Gassan <g_fox_fts@yahoo.com>
2020-10-30 22:33:02 +0100

    shareReplay

--- index.ts
+++ index.ts
@@ -1,34 +1,43 @@
 import './style.css';
 console.clear();
-
 // begin lesson code
-import { ObservableStore } from './store';
 
-const store = new ObservableStore({
-  user: 'joe',
-  isAuthenticated: true
-});
+import { fromEvent } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { shareReplay, mergeMapTo, share } from 'rxjs/operators';
 
-/*
- * Select a slice of state from store.
- */
-store.selectState('user').subscribe(console.log);
+const observer = {
+  next: val => console.log('next', val),
+  error: err => console.log('error', err),
+  complete: () => console.log('complete')
+};
 
-/*
- * Update a property with new value.
- */
-store.updateState({
-  user: 'bob'
-});
-
-store.updateState({
-  isAuthenticated: true
-});
+const click$ = fromEvent(document, 'click');
+const ajax$ = ajax('https://api.github.com/users/octocat');
 
 /*
- * Selected state above (user) only emits when value has changed
- * for the requested property.
+ * shareReplay turns a unicast observable into multicasted
+ * observable, utilizing a ReplaySubject behind the scenes.
+ * 
+ * In this example, we are mapping any clicks to an ajax
+ * request, sharing the response.
  */
-store.updateState({
-  isAuthenticated: false
-});
+const sharedClick$ = click$.pipe(
+  mergeMapTo(ajax$),
+  /*
+   * By default shareReplay shares all old values, like
+   * a standard ReplaySubject. In this case, we only want
+   * to share the most recent response.
+   */
+  shareReplay(1)
+);
+
+sharedClick$.subscribe(observer)
+
+/*
+ * Late subscribers will be replayed old value(s).
+ */
+setTimeout(() => {
+  console.log('subscribing');
+  sharedClick$.subscribe(observer);
+}, 5000);
